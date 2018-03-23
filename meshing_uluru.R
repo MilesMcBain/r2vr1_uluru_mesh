88a5e4e6b8869900b5d1c6ce894604b88d37527e
Miles McBain <miles.mcbain@gmail.com>
2018-03-23 11:04:52 +1000

    Triangulation code.

--- meshing_uluru.R
+++ meshing_uluru.R
@@ -2,6 +2,8 @@ library(sf)
 library(raster)
 library(tidyverse)
 
+##### Shapes #####
+
 # mymaps.google.com is a handy tool for determining bounding boxes.
 uluru_bbox_poly <- 
   st_bbox(c(ymin = -25.39521, 
@@ -36,3 +38,42 @@ uluru_outline <-
     main = "Uluru Elevation Contours")
   plot(uluru_outline["FEATTYPE"], 
     main = "Outline Contour")
+
+# check type of uluru outline
+uluru_outline$geometry
+
+as.matrix(uluru_outline$geometry[[1]])
+
+# convert to polygon
+uluru_outline_poly <- 
+  uluru_outline %>%
+  st_geometry() %>%
+  pluck(1) %>%
+  as.matrix() %>%
+  list() %>%
+  list() %>%
+  st_multipolygon() %>%
+  st_sfc()
+
+st_crs(uluru_outline_poly) <- st_crs(uluru_contours)
+
+##### Rasters #####
+nt_raster <- raster("./data/ELVIS_CLIP.tif")
+
+# sanity check - do these overlap?
+# convert uluru_outline_poly to raster CRS
+
+uluru_outline_poly <-
+  st_transform(uluru_outline_poly, crs(nt_raster)@projargs)
+
+# plot and overlap
+plot(nt_raster, main="Uluru outline over elevation raster")
+lines(as(uluru_outline_poly, 'Spatial'))
+
+
+##### Triangulating A Shape #####
+
+uluru_trimesh <- sf_to_trimesh(uluru_outline_poly, 2500)
+plot(uluru_trimesh, main = "Triangular mesh of Uluru outline polygon")
+
+##### Adding raster Elevation #####
